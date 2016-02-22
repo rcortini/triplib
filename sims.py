22d94999972157e085b8896edf9f6764e5d74180
Ruggero Cortini <rcortini@crg.eu>
2016-02-22 16:35:39 +0100

    new: simulations file

---
+++ sims.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def jump_to (p):
+    """
+    Select a site according to the probability vector p
+    """
+    value = np.random.random ()
+    return (np.abs(p-value)).argmin()
+
+def do_the_search (i0, nsteps, P) :
+    """
+    Perform a search of nsteps on the system described by the transition
+    matrix P, with a starting point given by i0.
+    """
+    nsites = len (sites)
+    visits = np.zeros (nsites).astype (int)
+    # init
+    i = i0
+    # cycle on steps
+    for t in range (nsteps) :
+        visits [i] += 1
+        i = jump_to (P [i])
+    return visits
