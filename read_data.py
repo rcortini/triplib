1b1c87b729d30748d42045fa40d11cc1d60a46e9
Ruggero Cortini <rcortini@crg.eu>
2016-02-25 11:36:10 +0100

    new: load_expr_binned function

--- read_data.py
+++ read_data.py
@@ -3,13 +3,18 @@ import numpy as np
 # module-wide variables
 datadir = "/home/rcortini/work/data/"
 hic_file = datadir + "chr2L.mtx"
+hic_file_normalized = datadir + "chr2L_normalized.mtx"
 gene_file = datadir + "Kc_exp_color.bed"
 colors_file = datadir + "colors.bed"
 reporter_file = datadir + "allprom.txt"
 
 # load hi-c data
-def load_hic () :
-    return np.loadtxt (hic_file)
+def load_hic (normalized=True) :
+    if normalized :
+        return np.loadtxt (hic_file_normalized)
+    else :
+        return np.loadtxt (hic_file)
+
 
 # load the gene data
 def load_genes () :
@@ -36,3 +41,27 @@ def load_reporters () :
         types.append ('f')
     reporter_dtype = {'names' : keys, 'formats' : types}
     return np.genfromtxt (reporter_file, dtype=np.dtype (reporter_dtype), skip_header=6)
+
+# get hic-binned average reporter expression
+def load_expr_binned (reporters, nsites,
+                      responsive = ['p8','p38','p39','p40'],
+                      hic_res=2000,
+                      substitute_nans=False) :
+    expr_binned = np.zeros (nsites)
+    n_reporters = np.zeros (nsites)
+    for r in reporters :
+        prom = r['prom']
+        if prom != 'p0' and prom in responsive : 
+            pos = r['pos']
+            i = pos/hic_res
+            try :
+                expr_binned [i] += r['nexp']
+                n_reporters [i] += 1.
+            except IndexError :
+                continue
+    # calculate the average per bin
+    with np.errstate (invalid='ignore') :
+        expr_binned/=n_reporters
+    if substitute_nans :
+        expr_binned [np.isnan (expr_binned)] = 0.
+    return expr_binned
