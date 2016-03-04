30088467246d3c31e9e58d9783e57a8ba473915c
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-03-04 15:59:06 +0100

    improved gene_expression_probability_matrix

--- gene_expression.py
+++ gene_expression.py
@@ -1,13 +1,16 @@
 import numpy as np
+from .data_process import warn_message
 
 # assign the promoters
 def site_idx (n, hic_res=2000) :
     return int (n/hic_res)
 
-def gene_expression_probability_matrix (nsites, genes, pfunc,
+def gene_expression_probability_matrix (nsites, genes, Pfunc,
                                         hic_res=2000, promoter_distance=500,
                                        n_exclude=0) :
     Pg = np.zeros ([nsites,nsites])
+    pfunc = Pfunc[0]
+    pfunc_args = Pfunc[1]
     for gene in genes :
         startsite = gene ['start']
         endsite = gene ['end']
@@ -20,17 +23,18 @@ def gene_expression_probability_matrix (nsites, genes, pfunc,
         startsite = site_idx (startsite, hic_res)
         endsite = site_idx (endsite, hic_res)
         try :
-            Pg [startsite,endsite] = pfunc (gene)
+            Pg [startsite,endsite] = pfunc (gene,pfunc_args)
         except IndexError :
+            warn_message ("gene_exp", "Index error with %d/%d" % (startsite,endsite))
             continue
     return Pg [n_exclude:,n_exclude:]
 
 # the firing function default: fire if gene present
-def always_fire (gene) :
+def always_fire (gene,args) :
     return 1.
 
 # fire if gene is active
-def fire_if_active (gene) :
+def fire_if_active (gene,args) :
     if gene ['expr'] != 0. :
         return 1.
     else :
