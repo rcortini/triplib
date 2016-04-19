d6aa84cc0b41d967467d73da7f09a3d3a793062b
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-04-19 10:15:44 +0200

    improved line_plot (faster)

--- visualize_results.py
+++ visualize_results.py
@@ -63,16 +63,20 @@ def plot_hic_matrix (H,ax,N1,N2,hic_res=2000) :
              interpolation='none')
 
 def line_plot (ax,xvals,yvals,N1=None,N2=None,show_xaxis=False) :
+    if N1 is not None and N2 is not None :
+        ax.set_xlim (N1,N2)
+        mask = np.logical_and(xvals>N1,xvals<N2)
+    else :
+        ax.set_xlim (min(xvals),max(xvals))
+        mask = np.ones_like(xvals,dtype=bool)
     # plot values
-    for x,y in zip(xvals,yvals) :
+    for x,y in zip(xvals[mask],yvals[mask]) :
         ax.add_artist(Line2D((x,x),(0,y),color='k',linewidth=1))
     # plot borders
     ymin = min(yvals)
     ymax = max(yvals)
     delta = ymax-ymin
-    ax.set_ylim (ymin-0.1*delta,ymax+0.05*delta)
-    if N1 is not None and N2 is not None :
-        ax.set_xlim (N1,N2)
+    ax.set_ylim (ymin-0.01*delta,ymax+0.01*delta)
     # plot style
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
